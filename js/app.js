37d76b509ff6ebeb8b1899c7304f20933258a6b4
vinicius-maker <viniviana75@hotmail.com>
2022-02-05 23:56:35 -0300

    codigo refatorado

--- js/app.js
+++ js/app.js
@@ -1,118 +1,141 @@
 const uri = 'http://makeup-api.herokuapp.com/api/v1/products.json';
 const uribackend = 'http://localhost:3000/posts';
 
-// retorna todos os produtos
-let produtosPromise = fetch(`${uribackend}`);
-produtosPromise.then((resposta) => {
-  resposta.json().then((arrayProdutos)=>{
-    getMarcas(arrayProdutos)
-    getTipos(arrayProdutos)
-    let div = productItem(arrayProdutos)
-    document.getElementById("catalog").innerHTML = div;
-  })
-})
+const productElement = document.querySelector(".catalog");
+const sortFilter = document.getElementById("sort-type");
+const brandFilter = document.getElementById("filter-brand");
+const typeFilter = document.getElementById("filter-type");
+const nameFilter = document.getElementById("filter-name");
 
-// captura todas as marcas e trata para que não venha haver repetidas
-function getMarcas(arrayProdutos) {
-  const marcasRepetidas = arrayProdutos.map((nomeMarca) => nomeMarca.brand)
-  
-  const marcas = marcasRepetidas.filter((el, i) => marcasRepetidas.indexOf(el) === i)
+let brandsValues = [];
+let typeValues = [];
+let products = "";
+let elementsChilds;
 
-  const select = marcas.map(marca => {return item = `<option value="${marca}">${marca}</option>`;});
-  
-  document.getElementById("filter-brand").innerHTML = select;
+(async () => {
+  let response = await fetch(`${uribackend}`);
+  loadProducts(await response.json(), sortFilter.value);
+})();
 
-  return select.join("");
+
+function loadProducts(json, sortType)
+{
+  let elements = sortProducts(json, sortType).map((product) => productItem(product)).join("");
+
+  productElement.innerHTML = elements;
+
+  elementsChilds = Array.from(productElement.querySelectorAll(".product"));
+
+  loadComboOptions(brandFilter, brandsValues.uniq().sort());
+  loadComboOptions(typeFilter, typeValues.uniq().sort());
+
+  products = json;
 }
 
-// captura todos os tipos de produtos e trata para que não venha haver repetidos
-function getTipos(arrayProdutos) {
-  const tiposRepetidos = arrayProdutos.map((nomeTipo) => nomeTipo.product_type)
-  
-  const tiposProdutos = tiposRepetidos.filter((el, i) => tiposRepetidos.indexOf(el) === i)
+nameFilter.addEventListener("keyup", loadFilters );
+brandFilter.addEventListener("change", loadFilters);
+typeFilter.addEventListener("change", loadFilters);
+sortFilter.addEventListener("change", (e) => {
+  loadProducts(products, sortFilter.value)
+  loadFilters();
+});
 
-  let select = tiposProdutos.map(tipoProduto => {return item = `<option value="${tipoProduto}">${tipoProduto}</option>`;});
+function loadFilters(){
 
-  document.getElementById("filter-type").innerHTML = select;
+  let name = nameFilter.value;
+  let brand = brandFilter.value;
+  let type = typeFilter.value;
 
-  //return select.join("");
+  elementsChilds.forEach((product) => {
+    if(validProduct(product, name, brand, type)){
+      product.style.display = "block"
+    } else {
+      product.style.display = "none"
+    }
+  });
 }
 
-// captura todos os preços e ordena do menor ao maior valor (mostra no console e precisa chamar a função)
-function getMenorValor(arrayProdutos) {
-  const precosProdutos = arrayProdutos.map((menorPreco) =>  parseFloat(menorPreco.price)).sort()
-  console.log(precosProdutos);
+function validProduct(product, name, brand, type) {
+
+  const search = new RegExp(name, "i");
+
+  const checkName = search.test(product.dataset.name);
+
+  const checkBrand = product.dataset.brand.includes(brand);
+
+  const checkType = product.dataset.type.includes(type);
+
+  return checkName && checkBrand && checkType;
 }
 
-// verifica o que foi selecionado no select do filtro
-function fitro() {
-  let filtro = document.getElementById('sort-type').value
-  console.log(filtro);
-}
-fitro();
 
-// ordena em ordem alfabetica os produtos
-function sortByName(arrayProdutos) {
-  let teste = arrayProdutos.sort((a, b) => {
-      let nameA = a.name.toUpperCase().replace(" ", "");
-      let nameB= b.name.toUpperCase().replace(" ", "");
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
-      return 0;
-    });
-    console.log(teste);
+function loadComboOptions(element, values) {
+  values.map((item) => element.insertAdjacentHTML("beforeend", `<option>${item}</option>`));
 }
-sortByName();
 
-// mostra os produtos no html
-function productItem(arrayProdutos) {
-let rows = arrayProdutos.map(arrayProduto => {
-  return item = `<div class="product" data-name=${arrayProduto.name} data-brand=${arrayProduto.brand} data-type=${arrayProduto.product_type} tabindex=${arrayProduto.id}>
+//EXEMPLO DO CÓDIGO PARA UM PRODUTO
+function productItem(product) {
+
+  brandsValues = brandsValues.concat([product.brand]);
+  typeValues = typeValues.concat([product.product_type]);
+
+  return `<div class="product" data-name="${product.name}" data-brand="${product.brand}" data-type="${product.product_type}" tabindex="${product.id}">
   <figure class="product-figure">
-    <img src=${arrayProduto.image_link} width="215" height="215" alt=${arrayProduto.name} onerror="javascript:this.src='img/unavailable.png'">
+    <img src="${product.image_link}" width="215" height="215" alt="${product.name}" onerror="javascript:this.src='img/unavailable.png'">
   </figure>
   <section class="product-description">
-    <h1 class="product-name">${arrayProduto.name}</h1>
-    <div class="product-brands"><span class="product-brand background-brand">${arrayProduto.brand}</span>
-<span class="product-brand background-price">R$ ${(arrayProduto.price * 5.50).toFixed(2).replace('.', ',')}</span></div>
+    <h1 class="product-name">${product.name}</h1>
+    <div class="product-brands"><span class="product-brand background-brand">${product.brand}</span>
+<span class="product-brand background-price">R$ ${parseFloat(product.price * 5.5).toFixed(2)}</span></div>
   </section>
-  ${loadDetails(arrayProduto)}
-</div>`;;
-});
-return rows.join("");
+  <section class="product-details">${loadDetails(product)}</section>
+</div>`;
 }
 
-// mostra os detalhes dos produtos no html
+//EXEMPLO DO CÓDIGO PARA OS DETALHES DE UM PRODUTO
 function loadDetails(product) {
-    return details = `<section class="product-details"><div class="details-row">
-        <div>Brand</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">${product.brand}</div>
-        </div>
-      </div><div class="details-row">
-        <div>Price</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">${(product.price * 5.50).toFixed(2).replace('.', ',')}</div>
-        </div>
-      </div><div class="details-row">
-        <div>Rating</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">${product.rating == null ? 0 : product.rating}</div>
-        </div>
-      </div><div class="details-row">
-        <div>Category</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">${product.category == null ? "" : product.category}</div>
-        </div>
-      </div><div class="details-row">
-        <div>Product_type</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">${product.product_type}</div>
-        </div>
-      </div></section>`;
+
+  let details = ["brand", "price", "rating", "category", "product_type"];
+
+  return Object.entries(product)
+  .filter(([name,value]) => details.includes(name))
+  .map(([name, value])=> 
+    `<div class="details-row">
+      <div>${name}</div>
+      <div class="details-bar">
+        <div class="details-bar-bg" style="width= 250">${value}</div>
+      </div>
+    </div>`).join("");
 }
 
+function sortProducts(products, sort){
+  switch (sort) {
+    case "Melhores Avaliados":
+      return products.sort((a, b) => 
+          a.rating > b.rating ? -1 : a.rating < b.rating ? 1 : 0 )
+    case "Menores Preços":
+      return products.sort((a, b) => 
+          parseFloat(a.price) > parseFloat(b.price) ? 1 : parseFloat(a.price) < parseFloat(b.price) ? -1 : 0 );
+    case "Maiores Preços":
+      return products.sort((a, b) => 
+          parseFloat(a.price) > parseFloat(b.price) ? -1 : parseFloat(a.price) < parseFloat(b.price) ? 1 : 0 );
+    case "A-Z":
+      return products.sort((a, b) => 
+          a.name > b.name ? 1 : a.name < b.name ? -1 : 0 );
+    case "Z-A":
+      return products.sort((a, b) => 
+        a.name > b.name ? -1 : a.name < b.name ? 1 : 0 );
+  }
+}
+
+
+
+Array.prototype.uniq = function () {
+  return this.filter(function (value, index, self) {
+    return self.indexOf(value) === index;
+  });
+};
+
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
